72236dd01f05f3075fa281b16fb85a00c0161dba
Maldero <kapelus96@gmail.com>
2015-08-28 17:49:43 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,6 +2,8 @@
 session_start();
 if(!isset($_SESSION['logged']))
   exit(header("Location: index.php"));
-else
-  exit(session_destroy()."You have been logged out.".header("refresh: 2; index.php"));
+else {
+  session_destroy();
+  exit("You have been logged out.".header("refresh: 2; index.php"));
+}
 ?>
