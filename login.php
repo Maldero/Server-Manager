db7183438653b2a774a3c0ae2580dd554292666d
Maldero <kapelus96@gmail.com>
2015-08-28 17:26:07 +0200

    Delete unnecessary variables.

--- login.php
+++ login.php
@@ -7,9 +7,7 @@ if(isset($_SESSION['logged'])) {
   header("Location: manager.php);
 }
 else {
-$user = $_POST['user'];
-$password = $_POST['password'];
-  if(empty($user) or empty($password)) {
+  if(empty($_POST['user']) or empty($_POST['password'])) {
     exit("Wrong user or password.".header("refresh: 2; index.php"));
   }
 }
