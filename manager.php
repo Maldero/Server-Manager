a6d9145a72f8e6c61a46e3cdbb94718db4037809
Maldero <kapelus96@gmail.com>
2015-08-28 20:14:26 +0200

    Update manager.php
    Version 1.0

--- manager.php
+++ manager.php
@@ -5,4 +5,21 @@ if(!isset($_SESSION['logged']))
   exit(header("Location: index.php"));
 ?>
 <html>
+    <head>
+      <title>Server Manager</title>
+      <link href="css/manager.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+      <div id="logged">
+        <?php echo "You're logged as <font color=\"blue\">".ucfirst($_SESSION['logged'])."</font>";?>
+      </div>
+      <div id="logout">
+        <a href="logout.php">
+          <input type="button" value="Logout"/>
+        </a>
+      </div>
+      <div id="infoUname">
+        <li>You're working on: <?php echo php_uname();?></li>
+      </div>
+    </body>
 </html>
