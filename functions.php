da72d3aad8f546b4f868076ded893d0398e6531e
Maldero <kapelus96@gmail.com>
2015-08-28 17:24:34 +0200

    Update functions.php
    Fill checkUserInPasswd function.

--- functions.php
+++ functions.php
@@ -1,9 +1,18 @@
 <?php
 function checkUserInPasswd($user) {
-  //TODO
+  $etcPasswd = file_get_contents('/etc/passwd');
+  $fileExplode = explode(chr(10), $etcPasswd);
+  $result = array_filter($fileExplode, 
+    function($var) use ($user) {
+      return preg_match("/\b$user+:x\b/i", $var);
+    });
+  if(count($result) > 0)
+    return true;
+  else
+    return false;
 }
 
-function checkUserInDatabase($user, passwd) {
+function checkUserInDatabase($user, $passwd) {
  //TODO
 }
 
