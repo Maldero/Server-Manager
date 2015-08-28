8b25c8a600578533093b7040e533f536718d72a2
Maldero <kapelus96@gmail.com>
2015-08-28 17:28:18 +0200

    Update login.php

--- login.php
+++ login.php
@@ -10,5 +10,7 @@ else {
   if(empty($_POST['user']) or empty($_POST['password'])) {
     exit("Wrong user or password.".header("refresh: 2; index.php"));
   }
+  if(checkUserInPasswd($_POST['user']) == false)
+    exit("Wrong user or password.".header("refresh: 2; index.php"));
 }
 ?>
