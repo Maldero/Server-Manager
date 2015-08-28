9bee390265d06dc3cbedffb95be849b0d0d88295
Maldero <kapelus96@gmail.com>
2015-08-28 16:09:06 +0200

    Update mysqlConfig.php

--- mysqlConfig.php
+++ mysqlConfig.php
@@ -2,7 +2,9 @@
 header('Content-Type: text/html; charset=utf-8');
 
 function mysqlConnection() {
-  @mysql_connect("127.0.0.1", "mngmtUser", "mngmtPassword") or die("Error while connecting ")
+  @mysql_connect("127.0.0.1", "mngmtUser", "mngmtPassword") or die("Error while connecting to the server.");
+  @mysql_select_db("mngmnt_db") or die("Error while selecting database.");
 }
 
+mysqlConnection();
 ?>
