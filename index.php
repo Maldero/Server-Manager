006a67f2c5bfbb91ccaa17a0728e6b8e34442746
Maldero <kapelus96@gmail.com>
2015-08-28 12:02:56 +0200

    Update index.php
    Version 1.0

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-<?php?>
+<?php
+require "mysqlConfig.php";
+session_start();
+
+if(isset($_SESSION['logged'])) {
+  header("Location: manager.php");
+}
+else {
+  echo '
+  <html>
+    <head>
+      <title>Login</title>
+      <link href="css/index.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+      <form method="POST" action="login.php">
+        <div>User: <input type="text" size="10" name="user"/></div>
+        <div>Password: <input type="password" size="10" name="password"/></br></div>
+        <input type="submit" value="Log in"/>
+      </form>
+    </body>
+  </html>';
+}
+?>
