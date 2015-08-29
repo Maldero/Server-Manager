021c2975361e3ada193ae681b721efae997a07fe
Maldero <kapelus96@gmail.com>
2015-08-29 02:08:18 +0200

    Update manager.php

--- manager.php
+++ manager.php
@@ -23,18 +23,22 @@ if(!isset($_SESSION['logged']))
         <li>You're working on: <font color="blue"><?php echo php_uname(); ?></font></li>
       </div>
     <?php if($_SESSION['logged'] == "Administrator") { ?>
-	  <div id="buttonsMain">
-		  <form method="POST" action="manager.php">
-		    <div id="shutdownButton">
-			    <?php if(isset($_POST['shutdown'])) { header("Location: manager.php"); shutdownButton(); } ?>
-			    <input type="image" name="shutdown" style="width:96px;" src="/img/shutdown.png"/><span>Shutdown</span>
-		    </div>
-		    <div id="rebootButton">
-			    <?php if(isset($_POST['reboot'])) { header("Location: manager.php"); rebootButton(); } ?>
-			    <input type="image" name="reboot" style="width:96px;" src="/img/reboot.png"/><span>Reboot</span>
-		    </div>
-		  </form>
+    <div id="buttonsMain">
+      <form method="POST" action="manager.php">
+        <div id="shutdownButton">
+          <?php if(isset($_POST['shutdown'])) { header("Location: manager.php"); shutdownButton(); } ?>
+          <input type="image" name="shutdown" style="width:96px;" src="/img/shutdown.png"/><span>Shutdown</span>
+        </div>
+        <div id="rebootButton">
+          <?php if(isset($_POST['reboot'])) { header("Location: manager.php"); rebootButton(); } ?>
+          <input type="image" name="reboot" style="width:96px;" src="/img/reboot.png"/><span>Reboot</span>
+        </div>
+      </form>
+      <form method="POST" action="manager.php">
+      	<?php if(isset($_POST['command'])) { header("Location: manager.php"); commandExec($_POST['command']); } ?>
+      	<div class="commandLine">Enter your command: <input type="text" name="command" size="15" placeholder="Command"></div>
+      </form>
 	  <?php } ?>
-	  </div>
+    </div>
     </body>
 </html>
