624a290af6dc96633e9f0f14bf15c21b56cb428c
Maldero <kapelus96@gmail.com>
2015-09-02 15:22:24 +0200

    Code correction

--- logout.php
+++ logout.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 if(!isset($_SESSION['logged']))
-  exit(header("Location: index.php"));
+	exit(header("Location: index.php"));
 else {
-  session_destroy();
+	session_destroy();
   exit("You have been logged out.".header("refresh: 1; index.php"));
 }
 ?>
