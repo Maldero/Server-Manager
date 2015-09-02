0dd30b65c3c5048c1b5f9119a1cd22c44e5bbf03
Maldero <kapelus96@gmail.com>
2015-09-02 15:24:48 +0200

    Code correction

--- mysqlConfig.php
+++ mysqlConfig.php
@@ -1,10 +1,8 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-
 function mysqlConnection() {
-  @mysql_connect("127.0.0.1", "mngmtUser", "mngmtPassword") or die("Error while connecting to the server.");
-  @mysql_select_db("mngmnt_db") or die("Error while selecting database.");
+  @mysql_connect("127.0.0.1", "root", "K@pelus123a") or die("Error while connecting to the server.");
+	@mysql_select_db("srwmgmnt") or die("Error while selecting database.");
 }
-
 mysqlConnection();
 ?>
