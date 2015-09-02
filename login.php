a742cf257210e8df3cb92e34702cd113583a09eb
Maldero <kapelus96@gmail.com>
2015-09-02 15:21:23 +0200

    Update login.php

--- login.php
+++ login.php
@@ -4,21 +4,22 @@ include "functions.php";
 session_start();
 
 if(isset($_SESSION['logged'])) {
-  header("Location: manager.php");
+	header("Location: manager.php");
 }
 else {
-  if(empty($_POST['user']) or empty($_POST['password'])) {
-    exit("Wrong user or password.".header("refresh: 2; index.php"));
-  }
-  if(checkUserInPasswd($_POST['user']) == false)
-    exit("Wrong user or password.".header("refresh: 2; index.php"));
-  else {
-    if(checkUserInDatabase($_POST['user'], $_POST['password']) !== false) {
-      $_SESSION['logged'] = $_POST['user'];
-      header("Location: manager.php");
-    }
-    else
-      exit("Wrong user or password.".header("refresh: 2; index.php"));
+	echo '<link href="css/login.css" rel="stylesheet" type="text/css">';
+	if(empty($_POST['user']) or empty($_POST['password'])) {
+		exit("<div class=\"Error\">Wrong user or password.".header("refresh: 2; index.php"));
+	}
+	if(checkUserInPasswd($_POST['user']) == false)
+		exit("Wrong user or password.".header("refresh: 2; index.php"));
+	else {
+		if(checkUserInDatabase($_POST['user'], $_POST['password']) !== false) {
+			$_SESSION['logged'] = $_POST['user'];
+			header("Location: manager.php");
+		}
+	else
+		exit("Wrong user or password.".header("refresh: 2; index.php"));
     }
 }
 ?>
