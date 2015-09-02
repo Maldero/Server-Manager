8db6b5d825faed093d0c2a98c4ad74ac0c58639f
Maldero <kapelus96@gmail.com>
2015-09-02 15:28:20 +0200

    Code correction

--- resourceMonitor.php
+++ resourceMonitor.php
@@ -7,98 +7,84 @@ $disk = shell_exec("df -h -t rootfs");
 $outputDisk = explode(" ", $disk);
 
 echo '
-	<html><div id="auto"><table id="rMonitTable1">
-		<tr>
-			<td>
-				</br>
-			</td>
-			<td>
-				'.ucfirst($outputMemory[27]).' (MB)
-			</td>
-			<td>
-				'.ucfirst($outputMemory[20]).'
-			</td>
-		</tr>
-		<tr>
-			<td>
-				RAM:
-			</td>
-			<td>
-				'.$outputMemory[66].'
-			</td>
-			<td>
-				'.$outputMemory[59] / 100 .'%
-			</td>
-		</tr>
-	</table>
-	<table id="rMonitTable2">
-		<tr>
-			<td>
-				</br>
-			</td>
-			<td>
-				'.ucfirst(substr($outputCPU[0], 1)).'
-			</td>
-			<td>
-				'.ucfirst(substr($outputCPU[4], 1)).'
-			</td>
-			<td>
-				'.ucfirst(substr($outputCPU[10], 1)).'
-			</td>
-		</tr>
-		<tr>
-			<td>
-				CPU:
-			</td>
-			<td>
-				'.$outputCPU[21].'%
-			</td>
-			<td>
-				'.$outputCPU[29].'%
-			</td>
-			<td>
-				'.$outputCPU[40].'%
-			</td>
-		</tr>
-	</table>
-	<table id="rMonitTable3">
-		<tr>
-			<td>
-				</br>
-			</td>
-			<td>
-				'.$outputDisk[8].'
-			</td>
-			<td>
-				'.$outputDisk[9].'
-			</td>
-			<td>
-				'.$outputDisk[10].'
-			</td>
-		</tr>
-		<tr>
-			<td>
-				Disk:
-			</td>
-			<td>
-				'.$outputDisk[25].'
-			</td>
-			<td>
-				'.$outputDisk[28].'
-			</td>
-			<td>
-				'.$outputDisk[31].'
-			</td>
-		</tr>
-	</table></div>
+	<html>
+		<div id="auto">
+			<table id="rMonitTable1">
+				<tr>
+					<td>
+						'.ucfirst($outputMemory[27]).' (MB)
+					</td>
+					<td>
+						'.ucfirst($outputMemory[20]).'
+					</td>
+				</tr>
+				<tr>
+					<td>
+						'.$outputMemory[66].'
+					</td>
+					<td>
+						'.$outputMemory[59] / 100 .'%
+					</td>
+				</tr>
+			</table>
+			<table id="rMonitTable2">
+				<tr>
+					<td>
+						'.ucfirst(substr($outputCPU[0], 1)).'
+					</td>
+					<td>
+						'.ucfirst(substr($outputCPU[4], 1)).'
+					</td>
+					<td>
+						'.ucfirst(substr($outputCPU[10], 1)).'
+					</td>
+				</tr>
+				<tr>
+					<td>
+						'.$outputCPU[21].'%
+					</td>
+					<td>
+						'.$outputCPU[29].'%
+					</td>
+					<td>
+						'.$outputCPU[40].'%
+					</td>
+				</tr>
+			</table>
+			<table id="rMonitTable3">
+				<tr>
+					<td>
+						'.$outputDisk[8].'
+					</td>
+					<td>
+						'.$outputDisk[9].'
+					</td>
+					<td>
+						'.$outputDisk[10].'
+					</td>
+				</tr>
+				<tr>
+					<td>
+						'.$outputDisk[25].'
+					</td>
+					<td>
+						'.$outputDisk[28].'
+					</td>
+					<td>
+						'.$outputDisk[31].'
+					</td>
+				</tr>
+			</table>
+		</div>
 	<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
 	<script type="text/javascript">
 	$(document).ready( function() {
 		setInterval(refresh, 250);
 	});
-	
+
 	function refresh() {
-			$("#auto").load(location.href + " #auto>*", "");
+		$("#auto").load(location.href + " #auto>*", "");
 	}
-	</script></html>';
+	</script>
+	</html>';
 ?>
